f49d4cc0e70e097a8addf7ed7ac901fe24504888
giammacarioca <giammattey@gmail.com>
2022-02-26 22:06:11 -0300

    initial commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+
+import QuotesList from '../../components/QuotesList'
+
+export default function Home() {
+  return (
+    <>
+      <h2>Quotes</h2>
+      <QuotesList />
+    </>
+  )
+}
